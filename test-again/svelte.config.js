5783edc0dc5d4e9f8dc5045ec836c3d92419d922
Franciss-prog <francisgil.prog@gmail.com>
2024-12-28 14:52:32 +0800

    sapa

--- test-again/svelte.config.js
+++ test-again/svelte.config.js
@@ -1,3 +1,4 @@
+import { version } from '$service-worker';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
@@ -11,7 +12,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			// Remove the `fallback` option if you don't want to use a fallback page
+			fallback: null,
+			version: 20,
+		})
 	}
 };
 
